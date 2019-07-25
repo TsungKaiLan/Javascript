73e55a9cc7e4949e3d3e252bc436e93f7f0c9b33
TsungKaiLan <spring56132@gmail.com>
2019-07-25 17:54:29 +0800

    0725

--- BlueTools.js
+++ BlueTools.js
@@ -1,9 +1,3 @@
-// function sayYaV6(welcome, ...names) {
-//     for (let name of names) {
-//         document.write(`${welcome}! ${name}<br>`);
-//     }
-// }
-
 function checkTWID(id) {
     let ret = false;
     let regex = /^[a-zA-Z][12][0-9]{8}$/;
@@ -37,4 +31,29 @@ function checkTEL(tel) {
     }
 
     return ret;
+}
+
+function createAnswer(n = 3) {
+    let poker = [];
+    for (let i = 0; i < 10; i++) poker[i] = i;
+    for (let i = poker.length - 1; i > 0; i--) {
+        let rand = parseInt(Math.random() * (i + 1));
+        [poker[i], poker[rand]] = [poker[rand], poker[i]];
+    }
+    let ret = '';
+    for (i = 0; i < n; i++) {
+        ret += poker[i];
+    }
+    return ret;
+}
+function checkAB(ans, gus) {
+    let a = 0, b = 0;
+    for (let i = 0; i < gus.length; i++) {
+        if (gus.charAt(i) == ans.charAt(i)) { //gus中的第i碼 == ans中的第i碼
+            a++;
+        } else if (ans.indexOf(gus.charAt(i)) >= 0) { //ans中的第i碼是否存在於a中
+            b++;
+        }
+    }
+    return `${a}A${b}B`;
 }
